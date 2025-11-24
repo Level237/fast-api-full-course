1428750f967e7b7eceea3f2a03b4114af5003a2a
level237 <bramslevel129@gmail.com>
2025-11-24 15:56:54 +0100

    feat: Implement a full CRUD API for books, replacing initial example routes and models.

--- main.py
+++ main.py
@@ -1,44 +1,101 @@
-from fastapi import FastAPI,Header
-from typing import Optional
+from fastapi import FastAPI,status
+from fastapi.exceptions import HTTPException
 from pydantic import BaseModel
+from typing import List
+
 app=FastAPI()
-
-@app.get("/")
-
-async def root():
-    return {"message": "Hello World"}
-
-@app.get("/greet")
-
-async def greet_name(name:Optional[str] = "User",age:int=0) -> dict:
-    return {"message": f"Hello {name} you are {age} years old"}
-
-
-class BookCreateModel(BaseModel):
-    title : str
-    author : str
-    
-@app.post("/create_book")
-
-async def create_book(book_data:BookCreateModel):
-    return {
-        "title":book_data.title,
-        "author": book_data.author
+books= [
+    {
+        "id":1,
+        "title":"Think python",
+        "author":"Al Sweigart",
+        "publisher":"O'Reilly Media",
+        "publisher_date":"2021-01-01",
+        "page_count":1234,
+        "language":"English"
+    },
+    {
+        "id":2,
+        "title":"Django by Example",
+        "author":"Antonio Mele",
+        "publisher":"level",
+        "publisher_date":"2021-03-01",
+        "page_count":1234,
+        "language":"English"
+    },{
+        "id":3,
+        "title":"Laravel by Example",
+        "author":"Antonio Mele",
+        "publisher":"Jimmy",
+        "publisher_date":"2021-03-01",
+        "page_count":1234,
+        "language":"English"
     }
-    
+]
 
-@app.get("/get_headers",status_code=201)
+class Book(BaseModel):
+    id: int
+    title: str
+    author: str
+    publisher: str
+    publisher_date: str
+    page_count: int
+    language: str
 
-async def get_headers(
-    accept:str =Header(None),
-    content_type:str = Header(None),
-    user_agent:str = Header(None),
-    host:str =Header(None)
-):
-    request_headers = {}
+class BookUpdateModel(BaseModel):
+    title: str
+    author: str
+    publisher: str
+    page_count: int
+    language: str
     
-    request_headers['Accept'] = accept
-    request_headers['Content-Type'] = content_type
-    request_headers['User-Agent'] =user_agent
-    request_headers['Host']=host
-    return request_headers
+@app.get("/books",response_model=List[Book])
+
+async def get_all_books() -> list:
+    return books
+
+@app.post("/books",status_code=status.HTTP_201_CREATED)
+
+async def create_a_book(book_data:Book) ->dict:
+    newBook=book_data.model_dump()
+    
+    books.append(newBook)
+    
+    return newBook
+
+@app.get("/book/{book_id}")
+
+async def get_book(book_id: int) -> dict:
+    for book in books:
+        if book["id"] == book_id:
+            return book
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Book not found")
+@app.get("/book/{book_id}")
+
+async def update_book(book_id: int) -> dict:
+    for book in books:
+        if book["id"] == book_id:
+            return book
+
+@app.patch("/book/{book_id}")
+
+async def update_book(book_id: int,book_data:BookUpdateModel) -> dict:
+    
+    for book in books:
+        if book['id'] == book_id:
+            book['title'] = book_data.title
+            book['author'] = book_data.author
+            book['publisher'] = book_data.publisher
+            book['page_count'] = book_data.page_count
+            book['language'] = book_data.language
+            return book
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Book not found")
+
+@app.delete("/book/{book_id}",status_code=status.HTTP_204_NO_CONTENT)
+
+async def delete_book(book_id: int):
+    for book in books:
+        if book["id"] == book_id:
+            books.remove(book)
+            return {}
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Book not found")
