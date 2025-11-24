d7b44ee97f46a9daa1b140fbcc92096308335d79
level237 <bramslevel129@gmail.com>
2025-11-24 16:22:31 +0100

    refactor: Modularize FastAPI book API by migrating data, schemas, and routes from `main.py` to `src/books`.

---
+++ src/books/book_data.py
@@ -0,0 +1,28 @@
+books= [
+    {
+        "id":1,
+        "title":"Think python",
+        "author":"Al Sweigart",
+        "publisher":"O'Reilly Media",
+        "publisher_date":"2021-01-01",
+        "page_count":1234,
+        "language":"English"
+    },
+    {
+        "id":2,
+        "title":"Django by Example",
+        "author":"Antonio Mele",
+        "publisher":"level",
+        "publisher_date":"2021-03-01",
+        "page_count":1234,
+        "language":"English"
+    },{
+        "id":3,
+        "title":"Laravel by Example",
+        "author":"Antonio Mele",
+        "publisher":"Jimmy",
+        "publisher_date":"2021-03-01",
+        "page_count":1234,
+        "language":"English"
+    }
+]
