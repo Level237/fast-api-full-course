9f2f462ab10715d63a700085417396d85428ae80
level237 <bramslevel129@gmail.com>
2025-11-25 18:20:36 +0100

    feat: Implement initial database setup with SQLModel, define Book model, integrate DB initialization into FastAPI lifespan, and update gitignore.

---
+++ src/books/model.py
@@ -0,0 +1,29 @@
+from sqlmodel import SQLModel,Field,Column
+from datetime import datetime
+import uuid
+import sqlalchemy.dialects.postgresql as pg
+
+
+class Book(SQLModel,table=True):
+    __tablename__ = "books"
+    
+    
+    uid: uuid.UUID = Field(
+        sa_column=Column(
+            pg.UUID(as_uuid=True),
+            primary_key=True,
+            default=uuid.uuid4,
+            nullable=False,
+        )
+    )
+    title: str
+    author: str
+    publisher: str
+    publisher_date: str
+    page_count: int
+    language: str
+    created_at: datetime=Field(sa_column=Column(pg.TIMESTAMP  ,default=datetime.now()))
+    updated_at: datetime=Field(sa_column=Column(pg.TIMESTAMP  ,default=datetime.now()))
+    
+    def __repr__(self):
+        return f"<Book {self.title}>"
