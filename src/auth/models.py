ec01a9ac3c8a2f4fc766af3ef36236ead03acb70
level237 <bramslevel129@gmail.com>
2025-11-26 17:42:25 +0100

    feat: introduce User SQLModel in auth module

---
+++ src/auth/models.py
@@ -0,0 +1,28 @@
+from sqlalchemy import SQLModel,Field,Column
+from sqlalchemy.dialects.postgresql import UUID as pgUUID
+import uuid
+import datetime
+
+  
+class User(SQLModel,table=True):
+    
+    __tablename__ = "users"
+    
+    uid:uuid.UUID = Field(
+        sa_column=Column(
+            pgUUID(as_uuid=True),
+            nullable=False,
+            primary_key=True,
+            default=uuid.uuid4,
+        )
+    )
+    username:str
+    email : str
+    first_name : str
+    last_name : str
+    is_verified : bool = Field(default=False)
+    created_at: datetime=Field(sa_column=Column(pg.TIMESTAMP,default=datetime.now()))
+    updated_at: datetime=Field(sa_column=Column(pg.TIMESTAMP,default=datetime.now()))
+    
+    def __repr__(self):
+        return f"<User {self.username}>"
